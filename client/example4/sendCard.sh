2509d51e264422b77ececd5c565e494d04b4f832
freddidierRTE <frederic-f.didier@rte-france.com>
2021-03-11 10:34:43 +0100

    Adapt getting started for future version 2.2.0

--- client/example4/sendCard.sh
+++ client/example4/sendCard.sh
@@ -1,4 +1,4 @@
 export current_date_in_milliseconds_from_epoch=$(($(date +%s%N)/1000000))
-export current_date_plus_one_day_in_milliseconds_from_epoch=$(($(date +%s%N)/1000000 + 86400000))
-export current_date_plus_one_day_plus_one_hour_in_milliseconds_from_epoch=$(($(date +%s%N)/1000000 + 90000000))
+export current_date_plus_two_hours_in_milliseconds_from_epoch=$(($(date +%s%N)/1000000 + 7200000))
+export current_date_plus_six_hours_in_milliseconds_from_epoch=$(($(date +%s%N)/1000000 + 21600000))
 curl -X POST http://localhost:2102/cards -H "Content-type:application/json" --data "$(envsubst <$1)"
