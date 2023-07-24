6ef3a2784c2ab4249a3a11adf416d8ed762f588c
Duchatelle Hubert <hubert.duchatelle@rte-france.com>
2023-07-24 14:21:26 +0200

    test
    Signed-off-by: Duchatelle Hubert <hubert.duchatelle@rte-france.com>

--- .github/scripts/licenceHeaderCheck.sh
+++ .github/scripts/licenceHeaderCheck.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+incorrect_files=()
+
+check_pattern() {
+    file="$1"
+    pattern="$2"
+    if ! grep -q -i -w "$pattern" "$file"; then
+        incorrect_files+=("$file")
+    fi
+}
+
 for file in "$@"; do
-    case $file in
+    case "$file" in
         *.ts | *.js | *.java | *.html | *.css | *.scss | *.handlebars | *.sh)
-            echo $file Licence MPL
+            check_pattern "$file" "copy"
             ;;
 
         *.adoc | *.svg)
-            echo $file Licence Creative Commons
+            check_pattern "$file" "right"
             ;;
     esac
 done
+
+if [ "${#incorrect_files[@]}" -gt 0 ]; then
+    echo "::error::Missing correct Copyright header in :"
+    for file in "${incorrect_files[@]}"; do
+        echo "$file"
+    done
+    exit 1
+fi
